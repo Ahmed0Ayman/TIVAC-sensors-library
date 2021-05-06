ff25ff4ed3cb7c491350333017f4d30e79585923
Ahmed Ayman <31223687+Ahmed0Ayman@users.noreply.github.com>
2021-05-06 21:22:48 +0200

    Add files via upload

---
+++ TM4C SENSORS/LCD_Driver/LCD.c
@@ -0,0 +1,319 @@
+
+#include "LCD.h"
+
+
+/* start position for x & y location as described in data sheet*/ 
+static uint8_t column_position [2] = {0x80,0xc0};
+
+
+/*
+ * brief  : this function used to convert integer to string 
+ * param. : num this is the integer value that you want to convert
+ * param. : BUFF pointer to buffer to hold the converted numbers
+ * return : void 
+ */
+static void int_to_str(uint64_t num , uint8_t* BUFF);
+	
+
+
+/*
+ * brief  : this function used to set enable bit in LCD
+ * return : void 
+ */
+static void LCD_Triger_Enable(void);
+
+
+
+/*
+ * brief  : this function used to send Data to LCD in 4 bit form 
+ * param. : data the specific byte that you want to send to LCD 
+ * return : void 
+ */
+static void LCD_Send_4BitData(uint8_t data)
+{
+	LCD_PORT->DATA |= ((data)& 0XF0);
+  LCD_Triger_Enable();
+	LCD_PORT->DATA &= 0X0F;
+	LCD_PORT->DATA |= ((data<<4)& 0xf0);
+  LCD_Triger_Enable();
+	LCD_PORT->DATA = 0X00;
+	if(data <= 0x04)
+	delayMs(2);
+	else 
+	delayUs(400);
+} /* END_FUN LCD_Send_4BitData()*/
+
+
+
+
+/*
+ * brief  : this function used to set enable bit in LCD
+ * return : void 
+ */
+static void LCD_Triger_Enable(void)
+{
+	LCD_PORT->DATA |= LCD_EN;
+	delayUs(400);
+	LCD_PORT->DATA &=~LCD_EN;
+	delayUs(400);
+	
+} /* END_FUN LCD_Triger_Enable()*/
+
+
+
+
+/*
+ *brief  : this function used to send an character to LCD with the current location 
+ * param. : Character the specific character to send to LCD 
+ * return : void 
+ */  
+void LCD_Send_Character_CurrLoc(uint8_t character)
+{
+	LCD_PORT->DATA  |=LCD_RS;
+	delayMs(1);
+	LCD_Send_4BitData(character);
+	
+}/* ECD_FUN LCD_Send_Character_CurrLoc */
+
+
+
+
+
+/*
+ * brief  : this function used to send command to LCD  
+ * param. : command the specific command to send to LCD chose one of @ LCD_COMMANED_ 
+ * return : void 
+ */
+void LCD_Send_Command(uint8_t command)
+{
+	
+	LCD_PORT->DATA  &=~LCD_RS;  /* remmeber to always connect LCD_RW to ground */
+	LCD_Send_4BitData(command);
+	
+	
+} /* END_FUN LCD_Send_Command()*/
+
+
+
+
+/*
+ *brief  : this function used to initialize (GPIO &LCD) 
+ * return : void 
+ */  
+void LCD_Initializaion(void)
+{
+SYSCTL->RCGCGPIO |= 0x01;    /* enable GPIOA clock */
+__NOP();__NOP();__NOP();    /* wait 3 cycles */
+LCD_PORT->DEN = 0xff;       /* set GPIOA as digital output */
+LCD_PORT->DIR = 0xff;       /* set GPIOA pins as output pins */
+
+	
+	delayMs(20);
+	
+	/* LCD Initialization command*/
+	LCD_Send_Command(0x33U);  
+	LCD_Send_Command(0x32U);
+	LCD_Send_Command(LCD_COMMANED2LINE_4_BIT_MODE);
+	LCD_Send_Command(LCD_COMMANED_CLEAR_CURSOR_OFF);
+	LCD_Send_Command(LCD_COMMANED_CLEAR_LCD);
+	
+	
+} /* END_FUN LCD_Initializaion()*/
+
+
+
+
+/*
+ * brief  : this function used to modify the current cursor location 
+ * param. :  y  is specify the new Y axises( 1-->16)
+ * param. :  x  is specify the new X axises( 1-->16)
+ * return : void 
+ */
+void LCD_Goto_Location(uint8_t y,uint8_t x)
+{
+	if((x<1) || (y<1) ||(x>16)||(y<16))  /* check if the chosen cursor is wrong */
+	LCD_Send_Command(0x80);
+	else
+	LCD_Send_Command(column_position[y-1]+(x-1));
+	
+}/* END_FUN LCD_Goto_Location()*/
+
+
+
+
+
+
+/*
+ * brief  : this function used to send an string to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16
+ * param. :  x  is specify x axises location start from 1-->16
+ * param. : StringOfCharacters pointer to string that you want to display
+ * return : void 
+ */
+void LCD_Send_String_WithLoc(uint8_t y, uint8_t x, char *StringOfCharacters)
+{
+
+	
+LCD_Goto_Location(y,x);
+while((*StringOfCharacters) != NULL)
+	{
+		LCD_Send_Character_CurrLoc(*StringOfCharacters);
+		StringOfCharacters ++;
+	}
+	
+} /* END_FUN Send_A_String_WithLoc()*/
+
+
+
+
+/*
+ * brief  : this function used to send an string to LCD with the current location 
+ * param. : StringOfCharacters pointer to string that you want to display
+ * return : void 
+ */
+void LCD_Send_String_With_CurrLoc(char *StringOfCharacters)
+{
+
+	while((*StringOfCharacters) != NULL)
+	{
+		LCD_Send_Character_CurrLoc(*StringOfCharacters);
+		StringOfCharacters ++;
+	}
+	
+}/* END_FUN LCD_Send_String_With_CurrLoc */
+
+
+
+
+/*
+ * brief  : this function used to send an 64 bit integer to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16
+ * param. :  x  is specify x axises location start from 1-->16
+ * param. : IntegerToDisplay this is the integer value that you want to display
+ * return : void 
+ */
+void LCD_Send_Integer_WithLoc(uint8_t y, uint8_t x, uint64_t IntegerToDisplay)
+{
+	
+	
+		uint8_t arr[20];
+		int_to_str(IntegerToDisplay,arr);
+		LCD_Send_String_WithLoc(y,x,(char *)arr);
+	
+	
+}/* END_FUN LCD_Send_Integer_WithLoc() */
+
+
+
+
+
+
+/*
+ * brief  : this function used to send an 16 bit integer to LCD with the current location 
+ * param. : IntegerToDisplay this is the integer value that you want to display
+ * return : void 
+ */
+void LCD_Send_Integer_CurrLoc(uint64_t IntegerToDisplay)
+{
+		uint8_t arr[20];
+		int_to_str(IntegerToDisplay,arr);
+		LCD_Send_String_With_CurrLoc((char*)arr);
+	
+	
+}/* END_FUN LCD_Send_Integer_CurrLoc()*/
+
+
+
+
+
+
+
+/*
+ * brief  : this function used to convert integer to string 
+ * param. : num this is the integer value that you want to convert
+ * param. : BUFF pointer to buffer to hold the converted numbers
+ * return : void 
+ */
+static void int_to_str(uint64_t num , uint8_t* BUFF)
+{
+		uint32_t num1=num;
+	  uint16_t  len=0,rem=0;
+		while(num1 != 0) /* measure the length of the given number */
+		{
+			len++;
+			num1 = num1/10;
+			
+		}
+		
+		*(BUFF+len)= NULL ;  /* put null this important when you deal with strings most c string stanerd library make null as terminate condition */
+		while(num != 0)
+		{
+			rem = num%10;
+			num = num/10;
+			*(BUFF+len-1)=rem + '0' ; /* convet hte measured number as an ascii code */ 
+			len--;
+		}
+		
+		
+}/* END_FUN int_to_str */
+
+
+
+
+
+/*
+ * brief  : this function used to send an floating point number to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16
+ * param. :  x  is specify y axises location start from 1-->16
+ * param. : FlaotToDisplay this is the integer value that you want to display
+ * param. : precision number of digits of the float section that you want to display 
+ * return : void 
+ */
+void LCD_Send_Float_Withloc(uint8_t y, uint8_t x ,  float FlaotToDisplay , uint16_t precision)
+{
+	LCD_Goto_Location(y,x);
+	
+	double after=10.0;
+
+	if (precision == 1 )		after = after *1;  /* chose the precision */
+
+	else if (precision==2)	after = 100.0;
+
+	else if (precision==3)	after = 1000.0;
+
+	else if (precision==4)	after = 10000.0;
+
+	else if (precision==5)	after = 100000.0;
+
+	else if (precision==6)	after = 1000000.0;
+
+	uint16_t intValue = (uint16_t)FlaotToDisplay;                    /* calculate the int value */
+	float diffValue = FlaotToDisplay - (float)intValue;              
+	uint16_t decimalValue = (uint16_t)(diffValue * after);			  /* calculate the float value */
+	
+	
+	LCD_Send_Integer_CurrLoc(intValue);
+	LCD_Send_Character_CurrLoc('.');
+	LCD_Send_Integer_CurrLoc(decimalValue);
+	
+} /* END_FUN LCD_Send_Float_Withloc()*/
+
+
+
+/* delay Ms milliseconds (16 MHz CPU clock) */
+void delayMs(int Ms)
+{
+int i, j;
+for(i = 0 ; i < Ms; i++) /* wait for Ms delay */
+for(j = 0; j < (16000000/4000); j++)/*each for loop 4 instructions means take 4 cycle per loop so 1ms will equal 16000 cycle or 4000 loop */
+{} /* do nothing for 1 ms */
+}
+/* delay Us microseconds (16 MHz CPU clock) */
+void delayUs(int Us)
+{
+int i, j;
+for(i = 0 ; i < Us; i++)/* wait for Us delay */
+for(j = 0; j < 4; j++)/* each loop require 4 cycles so at 16mhz 4 loops will generate 1Us delay */
+
+{} /* do nothing for 1 us */
+}
---
+++ TM4C SENSORS/LCD_Driver/LCD.h
@@ -0,0 +1,198 @@
+
+#ifndef __LCD__
+#define __LCD__
+
+#include "tm4c123gh6pm.h"
+#include "string.h"
+
+
+/* 
+ * this is a LCD 2*16 driver library you can choose which port by modify LCD_PORT you connect your lcd 
+ * for more information about how to use this driver please check attatchment  pdf file
+ * that contain all you need to start with this driver 
+*/ 
+
+
+
+
+
+/* no need to LCD_RW pin always write so connect this pin to ground */
+#define LCD_RS 0x04  
+#define LCD_EN 0x08   
+
+
+#define LCD_PORT 					GPIOA /* define which port lcd connected to */
+
+
+
+
+
+/*some of common commands that used with LCD*/
+#define LCD_COMMANED_CLEAR_LCD								0X01U
+#define LCD_COMMANED_RETURN_HOME							0X02U
+#define LCD_COMMANED_SHIFT_RIGHT							0X05U
+#define LCD_COMMANED_SHIFT_LEFT								0X07U
+#define LCD_COMMANED_CLEAR_CURSOR_ON					0X0AU
+#define LCD_COMMANED_CLEAR_CURSOR_OFF					0X0CU
+#define LCD_COMMANED2LINE_4_BIT_MODE          0x28U
+
+
+
+
+/*
+ *brief  : this function used to initialize (GPIO &LCD) 
+ * return : void 
+ */  
+void LCD_Initializaion(void);
+
+
+
+
+
+
+/* 
+ *brief  : this function used to send an character to LCD with the current location 
+ * param. : Character the specific character to send to LCD 
+ * return : void 
+ */  
+void LCD_Send_Character_CurrLoc(uint8_t character);
+
+
+
+
+
+
+/*
+ * brief  : this function used to send an character to LCD with the chosen location 
+ * param. :  y  is specify the new Y axises
+ * param. :  x  is specify the new X axises
+ * param. : character the specific character to send to LCD 
+ * return : void 
+ */
+void LCD_Send_Character_WithLoc(uint8_t y,uint8_t x,uint8_t character);
+
+
+
+
+
+/*
+ * brief  : this function used to send an string to LCD with the current location 
+ * param. : string the specific character to send to LCD 
+ * return : void 
+ */
+void LCD_Send_String_CurrLoc(uint8_t *string);
+
+
+
+
+
+/*
+ * brief  : this function used to send command to LCD  
+ * param. : command the specific command to send to LCD chose one of @ LCD_COMMANED_ 
+ * return : void 
+ */
+void LCD_Send_Command(uint8_t command);
+
+
+
+
+
+/*
+ * brief  : this function used to modify the current cursor location 
+ * param. :  y  is specify the new Y axises 1-->16
+ * param. :  x  is specify the new X axises 1-->16
+ * return : void 
+ */
+void LCD_Goto_Location(uint8_t x , uint8_t y);
+
+
+
+
+
+
+/*
+ * brief  : this function used to send an string to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16
+ * param. :  x  is specify y axises location start from 1-->16
+ * param. : StringOfCharacters pointer to string that you want to display
+ * return : void 
+ */
+void LCD_Send_String_WithLoc(uint8_t loc_x_, uint8_t loc_y_, char *StringOfCharacters);
+
+
+
+
+/*
+ * brief  : this function used to send an 16 bit integer to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16
+ * param. :  x  is specify y axises location start from 1-->16
+ * param. : IntegerToDisplay this is the integer value that you want to display
+ * param. : NumberOfDigits number of digits of the integer number that you want to display
+ * return : void 
+ */
+void LCD_Send_Integer_WithLoc(uint8_t loc_x_, uint8_t loc_y_, uint64_t IntegerToDisplay);
+
+/*
+ * brief  : this function used to send an floating point number to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16
+ * param. :  x  is specify y axises location start from 1-->16
+ * param. : IntegerToDisplay this is the integer value that you want to display
+ * param. : precision number of digits of the float section that you want to display 
+ * return : void 
+ */
+void LCD_Send_Float_withloc(uint8_t x, uint8_t y ,  float FlaotToDisplay ,uint16_t precision);
+
+
+
+
+/*
+ * brief  : this function used to send an 64 bit integer to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16
+ * param. :  x  is specify x axises location start from 1-->16
+ * param. : IntegerToDisplay this is the integer value that you want to display
+ * return : void 
+ */
+void LCD_Send_Integer_WithLoc(uint8_t loc_x_, uint8_t loc_y_, uint64_t IntegerToDisplay);
+
+
+
+
+/*
+ * brief  : this function used to send an floating point number to LCD with the chosen location 
+ * param. :  y  is specify y axises location start from 1-->16 
+ * param. :  x  is specify y axises location start from 1-->16
+ * param. : IntegerToDisplay this is the integer value that you want to display
+ * param. : NumberOfDigits number of digits of the integer number that you want to display 
+ * return : void 
+ */
+void LCD_Send_Float_Withloc(uint8_t y, uint8_t x ,  float number , uint16_t precision);
+
+
+
+/*
+ * brief  : this function used to send an 16 bit integer to LCD with the current location 
+ * param. : IntegerToDisplay this is the integer value that you want to display
+ * return : void 
+ */
+void LCD_Send_Integer_CurrLoc(uint64_t IntegerToDisplay);
+
+
+
+
+/*
+ * brief  : this function used to send an string to LCD with the current location 
+ * param. : StringOfCharacters pointer to string that you want to display
+ * return : void 
+ */
+void LCD_Send_String_With_CurrLoc(char *StringOfCharacters);
+
+
+
+
+/* this function used to generate delay in ms */
+void delayMs(int Ms);
+/* this function used to generate delay in Us */
+void delayUs(int Us);
+
+
+#endif
